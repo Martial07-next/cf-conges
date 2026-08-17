d20c826a926937d37c19d0034503d340bb4f2a93
Martial07-next <martial.erouart8@gmail.com>
2026-08-17 15:24:40 +0200

    Update Logo.js

--- components/Logo.js
+++ components/Logo.js
@@ -2,7 +2,7 @@ export default function Logo({ dark = false }) {
   return (
     <div className="flex items-center gap-2.5 select-none">
       <img
-        src={dark ? "/logo.png" : "/logo-light.png"}
+        src={"public/logo.png" : "public/logo-light.png"}
         alt="CF Réseaux"
         className="h-8 w-auto shrink-0"
       />
