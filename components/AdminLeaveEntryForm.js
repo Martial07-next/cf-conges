d0345b03c725d22b3f8b5fdd5dc4ac49269223bf
Martial07-next <martial.erouart8@gmail.com>
2026-08-18 14:56:00 +0200

    Update AdminLeaveEntryForm.js

--- components/AdminLeaveEntryForm.js
+++ components/AdminLeaveEntryForm.js
@@ -21,7 +21,7 @@ export default function AdminLeaveEntryForm({ users, leaveTypes }) {
     setError("");
     setLoading(true);
 
-    const res = await fetch("/api/admin/leave-entries", {
+    const res = await fetch("/api/leave-entries",{
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ userId, leaveTypeId, dateDebut, dateFin, demiJournee, motif }),
