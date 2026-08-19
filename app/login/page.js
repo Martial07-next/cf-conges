fe378e11e33cfb310dd7936320aca280e1ec9587
Martial07-next <martial.erouart8@gmail.com>
2026-08-19 15:01:30 +0200

    Update page.js

--- app/login/page.js
+++ app/login/page.js
@@ -4,7 +4,6 @@ import { useState } from "react";
 import { signIn } from "next-auth/react";
 import { useRouter, useSearchParams } from "next/navigation";
 import Link from "next/link";
-import Logo from "@/components/Logo";
 
 export default function LoginPage() {
   const router = useRouter();
