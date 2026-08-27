d6ec2939003cc0d89949a1e36854db58d57c487c
Martial07-next <martial.erouart8@gmail.com>
2026-08-27 09:40:04 +0200

    Update layout.js

--- app/(app)/layout.js
+++ app/(app)/layout.js
@@ -13,7 +13,7 @@ export default async function AppLayout({ children }) {
     <div className="flex flex-col md:flex-row min-h-screen bg-brand-cream">
       <Sidebar />
       <BugReportButton />
-      <main className="flex-1 min-w-0 px-4 py-5 md:px-10 md:py-10">
+      <main className="flex-1 min-w-0 px-4 py-5 md:px-10 md:py-10 md:ml-64">
         <div className="max-w-6xl mx-auto">{children}</div>
       </main>
     </div>
